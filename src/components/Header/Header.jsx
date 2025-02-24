dfb66843239fc1a8fdf8f9d74267b3d414ed399d
John <johnlouisstephenson@gmail.com>
2025-02-24 19:54:27 +0000

    Add basic styling for select

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,23 @@
 export default function Header({ createIdea, handleSort }) {
   return (
     <header className="header">
-      <div className="wrapper header-grid">
+      <div className="wrapper header-flex">
         <button className="create-button" onClick={createIdea}>
           Create New Idea
         </button>
-        <label htmlFor="sortSelect">Sort by:</label>
-        <select
-          id="sortSelect"
-          onChange={(e) => handleSort(e.target.value)}
-        >
-          <option value="">-- Select an option --</option>
-          <option value="titleAsc">Title (A - Z)</option>
-        </select>
+        <div>
+          <label className="sort-label" htmlFor="sortSelect">
+            Sort by:
+          </label>
+          <select
+            className="sort-menu"
+            id="sortSelect"
+            onChange={(e) => handleSort(e.target.value)}
+          >
+            <option value="">Select an option</option>
+            <option value="titleAsc">Title (A - Z)</option>
+          </select>
+        </div>
       </div>
     </header>
   );
